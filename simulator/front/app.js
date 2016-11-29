a795fd9e88829cc307a16d65eb2e360b5f1c09d2
takuseno <takuma.seno@gmail.com>
2016-11-30 01:49:50 +0900

    Implement drawing robot

--- simulator/front/app.js
+++ simulator/front/app.js
@@ -12,7 +12,7 @@ var status = {
 
 socket.on('motor:move', (msg) => {
     console.log(msg)
-    status.right_speed = msg.right
+    status.right_speed = msg.right - 30
     status.left_speed = msg.left
 })
 
@@ -32,9 +32,27 @@ socket.on('sensor:distant', (msg) => {
     socket.emit('sensor:response', {right: 100 - status.x, left: 100})
 })
 
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+const draw = (status) => {
+    ctx.setTransform(1, 0, 0, 1, 0, 0)
+    ctx.clearRect(0, 0, 600, 400);
+    ctx.strokeRect(400, 150, 20, 20)
+    drawRobot(status)
+}
+const drawRobot = (status) => {
+    ctx.setTransform(1, 0, 0, 1, 0, 0)
+    ctx.translate(300 + status.x, 200 + status.y)
+    ctx.rotate(status.rotate)
+    ctx.strokeRect(-10, -5, 20, 10)
+    ctx.setTransform(1, 0, 0, 1, 0, 0)
+}
+
 setInterval(() => {
-    status.x += Math.cos(status.rotate) * (status.right_speed / 10)
-    status.y += Math.sin(status.rotate) * (status.left_speed / 10)
+    status.x += Math.cos(status.rotate) * (status.right_speed / 100)
+    status.y += Math.sin(status.rotate) * (status.left_speed / 100)
+    status.rotate += Math.PI * (status.right_speed - status.left_speed) / 100 / 180
     var span = document.getElementById('text')
     span.innerHTML = JSON.stringify(status)
-}, 1000)
+    draw(status)
+}, 100)
