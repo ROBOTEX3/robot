357c1295754814f890dd23cb32f2d287647e131a
takuseno <takuma.seno@gmail.com>
2016-11-30 22:49:24 +0900

    Move obstacle

--- simulator/front/obstacles.js
+++ simulator/front/obstacles.js
@@ -1,9 +1,9 @@
 // {x, y, w, h}
 
 const obstacles = [
-    {x: 100, y: -50, w: 10, h: 50},
     {x: -100, y: 50, w: 10, h: 50},
-    {x: 100, y: 0, w: 10, h: 50}
+    {x: 100, y: 0, w: 10, h: 50},
+    {x: 150, y: 80, w: 10, h: 50}
 ]
 
 module.exports = obstacles
