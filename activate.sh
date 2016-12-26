f99ab56f286398a01fab83a4b47eb1bc9d446df6
senomatchingood <seno@matchingood.co.jp>
2016-12-26 14:56:54 +0900

    Fix julius

--- activate.sh
+++ activate.sh
@@ -2,4 +2,4 @@ insmod /home/pi/dvd/motor/imcs01_driver_JROBO/driver/urbtc.ko
 
 lsmod
 
-~/dvd/julius/julius-4.2.3/julius/julius -C ~/dvd/julius/julius-kits/grammar-kit-v4.1/SampleGrammars/rapiro/rapiro.jconf -module > /dev/null &
+~/dvd/julius/julius-4.2.3/julius/julius -C voice/rapiro.jconf -module > /dev/null &
